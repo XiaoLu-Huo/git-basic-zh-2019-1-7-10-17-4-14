1e565422d37d538ee3cba5cbf5ac84205881269f
huoxiaolu <1821100432@qq.com>
2021-09-02 22:42:55 +0800

    [huoxioalu] feat: 完成section1中practiceB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> result = new ArrayList<>(collection1);
+        List<String> collection = new ArrayList<>();
+        for (List<String> stringList : collection2) {
+            collection.addAll(stringList);
+        }
+        result.retainAll(collection);
+        return result;
     }
 }
