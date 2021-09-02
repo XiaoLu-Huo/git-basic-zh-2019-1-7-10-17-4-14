c8ce5f60798cdf21318353527bdf4ac412a4ee6f
huoxiaolu <1821100432@qq.com>
2021-09-03 01:15:34 +0800

    [huoxioalu] feat: 完成section2中practiceC

--- src/main/java/cn/school/thoughtworks/section2/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section2/PracticeC.java
@@ -1,12 +1,25 @@
 package cn.school.thoughtworks.section2;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     Map<String, Integer> countSameElements(List<String> collection1) {
         //实现练习要求，并改写该行代码。
-
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        collection1.forEach(s ->{
+            if(s.contains("-")|| s.contains(":")){
+                String[] key = s.split("-|:");
+                map.put(key[0], map.getOrDefault(key[0], 0) + Integer.parseInt(key[1]));
+            } else if (s.contains("[")) {
+                    String key = s.substring(0,s.indexOf("["));
+                    Integer value = Integer.valueOf(s.substring(s.indexOf("[") + 1,s.indexOf("]")));
+                    map.put(key,map.getOrDefault(key,0) + value);
+            } else {
+                map.put(s, map.getOrDefault(s, 0) + 1);
+            }
+        });
+        return map;
     }
 }
