f6f2eb07473635a53232289ae665c3e05460a91f
huoxiaolu <1821100432@qq.com>
2021-09-02 23:51:16 +0800

    [huoxioalu] refactor: 使用stream重构section1中practiceB

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,17 +1,27 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        List<String> result = new ArrayList<>(collection1);
-        List<String> collection = new ArrayList<>();
-        for (List<String> stringList : collection2) {
-            collection.addAll(stringList);
-        }
-        result.retainAll(collection);
-        return result;
+        Set<String> set = new LinkedHashSet<>();
+//        采用下面的这种方式，会出现乱序
+//        collection2.forEach(s -> {
+//            List<String> list = collection1.stream()
+//                    .filter(s::contains)
+//                    .collect(Collectors.toList());
+//            set.addAll(list);
+//        });
+        collection1.forEach(s -> {
+            collection2.forEach(res -> {
+                List<String> collect = res.stream()
+                        .filter(ans -> ans.contains(s))
+                        .collect(Collectors.toList());
+                set.addAll(collect);
+            });
+        });
+        return new ArrayList<>(set);
     }
 }
