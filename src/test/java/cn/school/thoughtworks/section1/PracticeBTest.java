aa5878092cc430d4f59192537a9fc96d907a4e8d
huoxiaolu <1821100432@qq.com>
2021-09-02 22:43:52 +0800

    [huoxiaolu] test: 在practiceB中增加测试——collection2中有多个元素

--- src/test/java/cn/school/thoughtworks/section1/PracticeBTest.java
+++ src/test/java/cn/school/thoughtworks/section1/PracticeBTest.java
@@ -3,6 +3,7 @@ package cn.school.thoughtworks.section1;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,6 +26,20 @@ public class PracticeBTest {
 
         List<String> result = practiceB.collectSameElements(collection1, collection2);
         List<String> expectedResult = Arrays.asList("a", "e", "f", "d");
+        assertEquals(result, expectedResult);
+    }
+
+    @Test
+    public void collect_same_elements_should_return_an_array_with_same_elements_in_two_collections_when_secondCollection_has_two_children() {
+        List<String> collection1 = Arrays.asList("a", "e", "h", "t", "f", "c", "g", "b", "d");
+        List<String> list = Arrays.asList("a", "d", "e", "f");
+        List<String> list2 = Arrays.asList("a", "d", "e", "b");
+        List<List<String>> collection2 = new ArrayList<>();
+        collection2.add(list);
+        collection2.add(list2);
+        System.out.println(collection2.size());
+        List<String> result = practiceB.collectSameElements(collection1, collection2);
+        List<String> expectedResult = Arrays.asList("a", "e", "f", "b", "d");
         assertEquals(result, expectedResult);
     }
 }
