cdf91207f467ecf1000c8c5da40854b04e4e39e0
huoxiaolu <1821100432@qq.com>
2021-09-03 00:17:03 +0800

    [huoxioalu] feat: 完成section1中practiceC，有两个测试用例

--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,14 +1,26 @@
 package cn.school.thoughtworks.section1;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return collection1.stream()
-                .filter(collection2.get("value")::contains)
-                .collect(Collectors.toList());
+        Set<String> set = new LinkedHashSet<>();
+        collection1.forEach(s -> {
+            collection2.keySet().forEach(res->{
+                List<String> collect = collection2.get(res)
+                        .stream()
+                        .filter(ans -> ans.contains(s))
+                        .collect(Collectors.toList());
+                set.addAll(collect);
+            });
+        });
+//        collection2.keySet().forEach(s -> {
+//            List<String> collect = collection1.stream().filter(collection2.get(s)::contains)
+//                    .collect(Collectors.toList());
+//            set.addAll(collect);
+//        });
+        return new ArrayList<>(set);
     }
 }
