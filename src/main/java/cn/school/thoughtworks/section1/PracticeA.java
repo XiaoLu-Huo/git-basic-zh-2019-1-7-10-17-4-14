1efecc8c43036705360a094b2d7b5df1d79fbfa0
huoxiaolu <1821100432@qq.com>
2021-09-02 22:41:36 +0800

    [huoxioalu] refactor: 使用stream重构section1中practiceA

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -1,12 +1,13 @@
 package cn.school.thoughtworks.section1;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
-        List<String> result = new ArrayList<>(collection1);
-        result.retainAll(collection2);
-        return result;
+        return collection1.stream()
+                .filter(collection2::contains)
+                .collect(Collectors.toList());
     }
 }
