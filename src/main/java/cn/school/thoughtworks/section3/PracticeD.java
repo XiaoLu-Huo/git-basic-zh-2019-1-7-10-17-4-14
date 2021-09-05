53a6b0f1b59408f3788962eea230c0ca670d2967
huoxiaolu <1821100432@qq.com>
2021-09-05 15:04:01 +0800

    [huoxiaolu] feat: 完成section3中practiceD

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -1,12 +1,39 @@
 package cn.school.thoughtworks.section3;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeD {
+
+    private String value;
+
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        Map<String, Integer> collectionC = new HashMap<>();
+        collectionA.forEach(s ->{
+            createMap(collectionC, s);
+        });
+        object.get("value").forEach(s -> {
+            extracted(collectionC, s);
+        });
+        return collectionC;
+    }
 
-        return null;
+    private void createMap(Map<String, Integer> collectionC, String s) {
+        if(s.contains("-")){
+            String key = s.split("-")[0];
+            value = s.split("-")[1];
+            collectionC.put(key, collectionC.getOrDefault(key, 0) + Integer.parseInt(value));
+        } else {
+            collectionC.put(s, collectionC.getOrDefault(s, 0) + 1);
+        }
+    }
+
+    private void extracted(Map<String, Integer> collectionA, String s) {
+        Integer value = collectionA.get(s);
+        if(collectionA.containsKey(s) && value >= 3) {
+            collectionA.put(s, value - value / 3);
+        }
     }
 }
