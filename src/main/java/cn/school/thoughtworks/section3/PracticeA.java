ad5e226379a0c57f8c6b13ab06fed0660af74347
huoxiaolu <1821100432@qq.com>
2021-09-05 14:39:34 +0800

    [huoxiaolu] refactor :将判断逻辑抽取为单独的方法，并优化打包

--- src/main/java/cn/school/thoughtworks/section3/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeA.java
@@ -1,6 +1,5 @@
 package cn.school.thoughtworks.section3;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -8,10 +7,14 @@ public class PracticeA {
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
         object.get("value").forEach(s -> {
-            if(collectionA.containsKey(s)) {
-                collectionA.put(s,collectionA.get(s) - 1);
-            }
+            extracted(collectionA, s);
         });
         return collectionA;
+    }
+
+    private void extracted(Map<String, Integer> collectionA, String s) {
+        if(collectionA.containsKey(s)) {
+            collectionA.put(s, collectionA.get(s) - 1);
+        }
     }
 }
