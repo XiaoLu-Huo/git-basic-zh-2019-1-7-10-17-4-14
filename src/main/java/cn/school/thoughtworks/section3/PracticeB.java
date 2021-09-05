d8ab67df9b28a49c9fdca7ca13e78f7ae8e8dc6d
huoxiaolu <1821100432@qq.com>
2021-09-05 14:49:52 +0800

    [huoxiaolu] feat: 完成了section3的practiceB

--- src/main/java/cn/school/thoughtworks/section3/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeB.java
@@ -4,9 +4,19 @@ import java.util.List;
 import java.util.Map;
 
 public class PracticeB {
+
     Map<String,Integer> createUpdatedCollection(Map<String,Integer> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
+        object.get("value").forEach(s -> {
+            extracted(collectionA, s);
+        });
+        return collectionA;
+    }
 
-        return null;
+    private void extracted(Map<String, Integer> collectionA, String s) {
+        Integer value = collectionA.get(s);
+        if(collectionA.containsKey(s) && value >= 3) {
+            collectionA.put(s, value - value / 3);
+        }
     }
 }
