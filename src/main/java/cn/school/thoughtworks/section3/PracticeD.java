5c6f988f4610574a99dcc0c47f6c289513faba58
huoxiaolu <1821100432@qq.com>
2021-09-05 15:24:33 +0800

    [huoxiaolu] refactor: 使用lambda表达式重构section3中practiceD

--- src/main/java/cn/school/thoughtworks/section3/PracticeD.java
+++ src/main/java/cn/school/thoughtworks/section3/PracticeD.java
@@ -6,24 +6,18 @@ import java.util.Map;
 
 public class PracticeD {
 
-    private String value;
-
     Map<String,Integer> createUpdatedCollection(List<String> collectionA, Map<String,List<String>> object) {
         //实现练习要求，并改写该行代码。
         Map<String, Integer> collectionC = new HashMap<>();
-        collectionA.forEach(s ->{
-            createMap(collectionC, s);
-        });
-        object.get("value").forEach(s -> {
-            extracted(collectionC, s);
-        });
+        collectionA.forEach(s -> createMap(collectionC, s));
+        object.get("value").forEach(s -> extracted(collectionC, s));
         return collectionC;
     }
 
     private void createMap(Map<String, Integer> collectionC, String s) {
         if(s.contains("-")){
             String key = s.split("-")[0];
-            value = s.split("-")[1];
+            String value = s.split("-")[1];
             collectionC.put(key, collectionC.getOrDefault(key, 0) + Integer.parseInt(value));
         } else {
             collectionC.put(s, collectionC.getOrDefault(s, 0) + 1);
